74765c980a195a54b817a64a3b75dcb4b0f576c2
Sprinto14 <gefoster@hotmail.co.uk>
2023-12-08 00:28:35 +0000

    Day pt2 Completed!!

--- Day3/Day3_pt2.c
+++ Day3/Day3_pt2.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAXVAL 3
+#define LINESIZE 256
 
 void rotateArr(int *arr, int arrLen) {
     /// Move all values in an array down one index, putting the first value at the end.
@@ -20,15 +20,12 @@ void rotateArr(int *arr, int arrLen) {
 
 
 bool isNum(char c) {
-
-    //printf("Checking. ");
+    /// Boolean output for whether the input character is a number digit 0-9
 
     if ((c >= '0') && (c <= '9')) {
-        //printf("%c is number. ", c);
         return true;
     }
     
-    //printf("%c is not number. ", c);
     return false;
 
 }
@@ -58,7 +55,7 @@ Tuple findNumAtIndex(char *str, int index) {
     }
 
 
-    char numStr[8];
+    char numStr[8] = "\0\0\0\0\0\0\0\0";
     int leftIndex, rightIndex;
 
     // Scan left
@@ -98,14 +95,12 @@ Tuple findNumAtIndex(char *str, int index) {
     s.a = atoi(numStr);
     s.b = rightIndex;
 
-    //printf("Num %d ends at %d. ", s.a, s.b);
-
     return s;
 
 }
 
 
-int scanLine(char inputLines[3][256], int *index) {
+int scanLine(char inputLines[3][LINESIZE], int *index) {
     /// Scan the current line to find valid numbers
 
     int output = 0;
@@ -114,13 +109,9 @@ int scanLine(char inputLines[3][256], int *index) {
     char *currentLine = inputLines[index[1]];
     int lineLen = strlen(currentLine);
 
-    //printf("cl %s", currentLine);
-
     for (int i = 0; i < lineLen; i++) {
 
         if (currentLine[i] != '*') {continue;}
-
-        //printf("Found * at index %d - ", i);
 
         // Search surrounding area for numbers
 
@@ -133,11 +124,10 @@ int scanLine(char inputLines[3][256], int *index) {
 
             leftIndex = i-1;
 
-            if (isNum(currentLine[leftIndex])) {
-                
-                //printf("Number to left: %c\n", currentLine[leftIndex]);
+            // We're not at the boundary so check directly to the left of the *
+            if (isNum(currentLine[i-1])) {
 
-                Tuple numWithLocation = findNumAtIndex(currentLine, leftIndex);
+                Tuple numWithLocation = findNumAtIndex(currentLine, i-1);
                 surroundingNums[numsFound++] = numWithLocation.a;
                 
             }
@@ -147,15 +137,15 @@ int scanLine(char inputLines[3][256], int *index) {
             leftIndex = 0;
         }
 
+        // Right boundary condition
         if (i + 1 < strlen(currentLine)) {
 
             rightIndex = i + 2;
             
-            if (isNum(currentLine[rightIndex])) {
+            // We're not at the boundary so check directly to the right of the *
+            if (isNum(currentLine[i+1])) {
 
-                //printf("Number to right: %c\n", currentLine[leftIndex]);
-
-                Tuple numWithLocation = findNumAtIndex(currentLine, rightIndex);
+                Tuple numWithLocation = findNumAtIndex(currentLine, i+1);
                 surroundingNums[numsFound++] = numWithLocation.a;
 
             }
@@ -165,16 +155,26 @@ int scanLine(char inputLines[3][256], int *index) {
             rightIndex = strlen(currentLine);
         }
 
-        //printf("L%dR%d ", leftIndex, rightIndex);
 
-        // Scan below and above (separately so we can avoid counting the same number twice)
+        // Scan above and below (separately so we can avoid counting the same number twice)
+        for (int j = leftIndex; j < rightIndex; j++) {
+            
+            char charAbove = inputLines[index[0]][j];
+
+            if (isNum(charAbove)) {
+                
+                Tuple numWithLocation = findNumAtIndex(inputLines[index[0]], j);
+                surroundingNums[numsFound++] = numWithLocation.a;
+                j = numWithLocation.b; // Move our search a space after the previous number ended (only relevant if leftIndex and (rightIndex - 1) are separate numbers)
+
+            }
+        }
+        
         for (int j = leftIndex; j < rightIndex; j++) {
 
             char charBelow = inputLines[index[2]][j];
 
             if (isNum(charBelow)) {
-
-                //printf("Number below at index %d: %c\n", j, charBelow);
 
                 Tuple numWithLocation = findNumAtIndex(inputLines[index[2]], j);
                 surroundingNums[numsFound++] = numWithLocation.a;
@@ -183,46 +183,30 @@ int scanLine(char inputLines[3][256], int *index) {
             }
         }
 
-        for (int j = leftIndex; j < rightIndex; j++) {
-            
-            char charAbove = inputLines[index[0]][j];
-
-            if (isNum(charAbove)) {
-
-                //printf("Number above at index %d: %c\n", j, charAbove);
-                
-                Tuple numWithLocation = findNumAtIndex(inputLines[index[0]], j);
-                surroundingNums[numsFound++] = numWithLocation.a;
-                j = numWithLocation.b; // Move our search a space after the previous number ended (only relevant if leftIndex and (rightIndex - 1) are separate numbers)
-
-            }
-        }
+        
 
         // Only add the output if there are exactly 2 numbers around a cog
+        
 
+        /*
+        // Debug output - all numbers found
         printf("Numbers found: ");
         for (int k = 0; k < numsFound; k++) {
 
             printf("%d, ", surroundingNums[k]);
 
         }
-        //printf("\n");
-
+        printf("\n");
+        */
+        
         if (numsFound == 2) {
-
-            printf("\t- 2 found: adding to output\n");
 
             output += surroundingNums[0] * surroundingNums[1];
         
-        } else if (numsFound == 0) {
-            printf("No surrounding numbers found.\n");
-        } else {
-            printf("\t- Wrong number\n");
-        }
+        } 
 
     }
 
-    printf("\t\t\t\tTotal for this line: %d\n", output);
 
     return output;
 }
@@ -230,34 +214,33 @@ int scanLine(char inputLines[3][256], int *index) {
 int main() {
 
     FILE* fp;
-    char line[256];
+    char line[LINESIZE];
 
     char *fileName = "D:/Code/AoC23/Day3/Day3_input.txt";
     fp = fopen(fileName, "r");
 
     if (NULL == fp) {
-        //printf(stderr, "can't open %s: %s\n", fileName, strerror(errno));
-
-        //printf("File pointer not found.");
+        printf("File pointer not found.");
         fclose(fp);
         return -1;
     }
 
-    //printf("File opened!\n");
+    printf("File opened!\n");
 
 
     int total = 0;
 
-    char emptyLine[256];
-    for (int i = 0; i < 256; i++) {
-        emptyLine[i] = '.';
-    }
+    char emptyLine[LINESIZE];
+    memset(emptyLine, '.', LINESIZE);
+    emptyLine[LINESIZE - 2] = '\n';
+    emptyLine[LINESIZE - 1] = '\0';
 
     // We store 3 lines at a time
-    char inputLines[3][256];
+    char inputLines[3][LINESIZE];
 
     // Initial setup requires an additional empty line above the first input line
-    memmove(inputLines[0], emptyLine, 256);
+    memcpy(inputLines[0], emptyLine, LINESIZE);
+
 
     // Array of indices - these will rotate through to minimise memory wastage moving around each line of input
     // The central index (1) represents the current line we're scanning, 0 the line above, and 2 the line below.
@@ -265,27 +248,34 @@ int main() {
 
     // Place the first input line in the central position
     fgets(line, sizeof(line), fp);
-    memmove(inputLines[1], emptyLine, 256);
+    memcpy(inputLines[1], emptyLine, LINESIZE);
     strcpy(inputLines[1], line);
 
     while(fgets(line, sizeof(line), fp)) {
-
+        
         // Copy in the new line in the appropriate location (index[0] is the prev line, index[1] the current line, so index[2] is the next line)
-        memmove(inputLines[index[2]], emptyLine, 256);
+        memcpy(inputLines[index[2]], emptyLine, LINESIZE);
         strcpy(inputLines[index[2]], line);
 
+        /*
+        // Debug output - show scan lines
+        //printf("\n0 %s", inputLines[index[0]]);
+        //printf("1 %s", inputLines[index[1]]);
+        //printf("2 %s", inputLines[index[2]]);
+        */
+        
         // Scan the current line to find numbers, storing the start and end location of a number when we find one
         total += scanLine(inputLines, index);
         
         // Rotate around the indices for which line we are scanning
         rotateArr(index, 3);
-        //printf("\nRunning Total: %d\n", total);
     }
 
-    
-    memmove(inputLines[index[2]], emptyLine, 256);
+    // Scan final line (need to add empty line after for scanning to work correctly)
+    memcpy(inputLines[index[2]], emptyLine, LINESIZE);
     total += scanLine(inputLines, index);
 
+    // Output
     printf("\nTotal: %d\n", total);
 
     return 0;
